3e122e6a65d745bc918dd086117d3e816ffe0e49
nathantaitano <nathantaitano@gmail.com>
2017-12-05 09:51:24 -0500

    Validate with 3000m x 3000m test data (from last night).
    100x100 cell xyz raster at 30m resolution. Ran in ~15 seconds (10000
    datapoints). Suggests 5 hrs for 30km x 30km, if scales linearly (not
    sure that it does - need to check w/ bigger test data). Plotted ggplot
    output w/ raster in outputs/3kSquareTestResult_2017-12-05.jpeg. It
    would be better to use real GIS software for this. Shockingly, my code
    made at 3am last night worked...

--- fun.R
+++ fun.R
@@ -1,13 +1,15 @@
-test2Xyz <- testXyz[1,]
-
 # x refers to latitude, y refers to longitude. 
 # This is in line with the lat/long order given by the xyz data format.
 
-for(r in 2:nrow(testXyz)){
-  if(r %% 3612 < 101 & r %% 3612 > 0){
-    test2Xyz <- rbind(test2Xyz, testXyz[r,])
-  }
-}
+# Just a modulo statement for extracting a subset square from an xyz gis dataframe, 
+# which itself was read directly in with read.table()
+# This gave me a 100x100 square at ~30m resolution in xyz format (test2Xyz)
+# To use in testing the functions while I was building them
+# for(r in 2:nrow(testXyz)){
+#   if(r %% 3612 < 101 & r %% 3612 > 0){
+#     test2Xyz <- rbind(test2Xyz, testXyz[r,])
+#   }
+# }
 
 # Shouldn't there be some better way to do this?
 getAdjs <- function(row, xLen, yLen){
@@ -44,7 +46,9 @@ getAdjs <- function(row, xLen, yLen){
 calcKcals <- function(startElev, endElev, dist, weight = 67){
   grade <- (endElev - startElev)/dist
   if(grade < 0){grade <- 0}
-  speed <- (26.8*3)*exp(-0.4*(grade)) # http://mtntactical.com/research/walking-uphill-10-grade-cuts-speed-13not-12/
+  # Walking speed by grade equation provided by mountain tactical:
+  # http://mtntactical.com/research/walking-uphill-10-grade-cuts-speed-13not-12/
+  speed <- (26.8*3)*exp(-0.4*(grade)) 
   # Equations & constants below provided by Melissa Joyce McGranahan, who took them from the ACSM website:
   # https://certification.acsm.org/metabolic-calcs
   # Most accurate for speeds of 50 – 100 m/min (1.9-3.7 mph)
@@ -82,27 +86,102 @@ findNode <- function(xyzTab, lat, long, xLen){
   xJump <- round((lat - xMin)/degRes,0)
   yJump <- round((yMax - long)/degRes,0)
   node <- xJump + xLen * yJump
+  return(node)
 }
 
+# This implementation of Dijkstra's algorithm has a couple of features intended to save RAM and comp time
+# First, it does not calculate calories in a complete navmesh: 
+#   instead, it calculates only the lengths between adjacent nodes
+# Second, it does not store a complete path for each visited node while running:
+#   instead, it stores only the previous node for each visited node, and backtracks to find the final path afterwards
+# Third, adjacent nodes are calculated using map dimensions
+#   and the consistent order of the xyz table rows (see getAdjs() above)
 dijkstra <- function(xyzFile, oriLat, oriLong, destLat, destLong, fileXLen, fileYLen, res=30){
+  # Part 1: set-up from original node and take first step
   xyzDf <- read.table(xyzFile)
   oriNode <- findNode(xyzDf, oriLat, oriLong, fileXLen)
   destNode <- findNode(xyzDf, destLat, destLong, fileXLen)
   
   unvisited <- 1:nrow(xyzDf)
-  dists <- rep(Inf, 1:nrow(xyzDf))
+  dists <- rep(Inf, nrow(xyzDf))
+  prevSteps <- as.integer(rep(NA, nrow(xyzDf)))
+  
   visited <- c(oriNode)
+  unvisited <- unvisited[-oriNode]
   dists[oriNode] <- 0
   curNode <- oriNode
-  adjNodes <- getAdjs
+  
+  adjNodes <- getAdjs(curNode, xLen = fileXLen, yLen = fileYLen)
   adjNodes <- adjNodes[!is.na(adjNodes)]
   diagAdjs <- adjNodes[names(adjNodes) %in% c("nw","ne","sw","se")]
   horAdjs <- adjNodes[names(adjNodes) %in% c("n","s","e","w")]
-  tentDists <- c()
-  tentDists <- c(tentDists, sapply(horAdjs, ))
+  horDists <- sapply(horAdjs, function(x) getHorCals(xyzDf[curNode,3],xyzDf[x,3]))
+  diagDists <- sapply(diagAdjs, function(x) getDiagCals(xyzDf[curNode,3],xyzDf[x,3]))
+  adjNodes <- c(diagAdjs, horAdjs)
+  adjDists <- c(diagDists, horDists)
+  dists[adjNodes] <- adjDists
+  prevSteps[adjNodes] <- curNode
+  
+  if(length(adjDists[adjDists == min(adjDists)]) > 1){
+    curNode <- sample(adjNodes[adjDists == min(adjDists)],1) # sample ties
+  }else{curNode <- adjNodes[adjDists == min(adjDists)]}
+  curNode <- unname(curNode)
+  visited <- c(visited,curNode)
+  unvisited <- unvisited[-which(unvisited %in% curNode)]
+  
+  # Part 2: Keep running algorithm until the destination node is visited
   while(!destNode %in% visited){
-    if()
-    unvisited <- unvisited[which(unvisited %in% visited)]
+    if(sum(!is.infinite(dists)) == 0){stop("Route blocked.")}
+    if(length(visited) %% 5000 == 0){print(paste(length(visited)/length(dists)*100, "% of nodes visited"))}
+    
+    adjNodes <- getAdjs(curNode, xLen = fileXLen, yLen = fileYLen)
+    adjNodes <- adjNodes[!is.na(adjNodes) & !adjNodes %in% visited]
+    diagAdjs <- adjNodes[names(adjNodes) %in% c("nw","ne","sw","se")]
+    horAdjs <- adjNodes[names(adjNodes) %in% c("n","s","e","w")]
+    
+    horDists <- c()
+    diagDists <- c()
+    if(length(horAdjs) > 0){
+      horDists <- sapply(horAdjs, function(x) getHorCals(xyzDf[curNode,3],xyzDf[x,3])) +
+        dists[curNode]
+    }
+    if(length(diagAdjs) > 0){
+      diagDists <- sapply(diagAdjs, function(x) getDiagCals(xyzDf[curNode,3],xyzDf[x,3])) +
+        dists[curNode]
+    }
+    
+    if(length(diagAdjs) > 0 | length(horAdjs) > 0){
+      adjNodes <- c(diagAdjs, horAdjs)
+      adjDists <- c(diagDists, horDists)
+      
+      for(d in 1:length(adjDists)){
+        if(adjDists[d] < dists[adjNodes[d]]){
+          dists[adjNodes[d]] <- adjDists[d]
+          prevSteps[adjNodes[d]] <- curNode
+        }else next
+      }
+    }
+    
+    ### Fix me in terms of unvisited. 
+    ### I want the unvisited node with the minimum value in dists
+    if(sum(dists[unvisited]==min(dists[unvisited])) > 1){
+      curNode <- sample(which(1:length(dists) %in% unvisited & dists == min(dists[unvisited])), 1)
+    }else{curNode <- which(1:length(dists) %in% unvisited & dists == min(dists[unvisited]))}
+    
+    unvisited <- unvisited[-which(unvisited %in% curNode)]
+    visited <- c(visited, curNode)
   }
   
+  # Part 3: backtrack from destination node to original to return path
+  backPath <- c(destNode)
+  backDists <- c() # for test (sum of each )
+  prev <- prevSteps[destNode]
+  while(!oriNode %in% backPath){
+    backPath <- c(backPath, prevSteps[prev])
+    backDists <- c(backDists,dists[prev])
+    prev <- prevSteps[prev]
+  }
+  
+  return(list(
+    calsToDest = dists[destNode], pathToDest = rev(backPath)))
 }
